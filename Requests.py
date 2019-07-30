9cf0602490baca4c0a0b40eb5462721ac0d75eba
Damien Kane Rigden <damien.rigden@gmail.com>
2019-07-30 14:56:12 -0400

    Added "Hello world!"

--- Requests.py
+++ Requests.py
@@ -4,6 +4,17 @@
 Created on Mon Jul 29 12:30:28 2019
 
 @author: damienrigden
+
+This file will contain examples that may be considered spoilers, but using this will not exclude you from winning the prize.
+Using this or not is just personal preference.
+
 """
 
 Can you encode "Hello world!"
+
+h = "Hello world!"
+
+encrypt = asleep(h)
+
+print(encrypt)
+{135: [6], 214: [7, 7], 138: [8], 220: [10, 10], 140: [11, 15], 145: [13], 209: [14], 225: [15], 130: [16], 199: [19, 6]}
